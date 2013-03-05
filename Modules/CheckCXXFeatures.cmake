eb75ad96a2c192a4756aa9c777a4d4904274410a
Rolf Eike Beer <kde@opensource.sf-tec.de>
2013-03-05 10:23:57 +0100

    rename to CheckCXXFeatures
    This can be used to check older and newer features, too. No need to restrict
    it to C++11 in the name.

--- Modules/CheckCXXFeatures.cmake
+++ Modules/CheckCXXFeatures.cmake
@@ -1,26 +1,26 @@
-# - Check which parts of the C++11 standard the compiler supports
+# - Check which features of the C++ standard the compiler supports
 #
 # When found it will set the following variables
 #
 #  CXX11_COMPILER_FLAGS         - the compiler flags needed to get C++11 features
 #
-#  HAS_CXX11_AUTO               - auto keyword
-#  HAS_CXX11_CLASS_OVERRIDE     - override and final keywords for classes and methods
-#  HAS_CXX11_CONSTEXPR          - constexpr keyword
-#  HAS_CXX11_CSTDINT_H          - cstdint header
-#  HAS_CXX11_DECLTYPE           - decltype keyword
-#  HAS_CXX11_FUNC               - __func__ preprocessor constant
-#  HAS_CXX11_INITIALIZER_LIST   - initializer list
-#  HAS_CXX11_LAMBDA             - lambdas
-#  HAS_CXX11_LONG_LONG          - long long signed & unsigned types
-#  HAS_CXX11_NULLPTR            - nullptr
-#  HAS_CXX11_RVALUE_REFERENCES  - rvalue references
-#  HAS_CXX11_SIZEOF_MEMBER      - sizeof() non-static members
-#  HAS_CXX11_STATIC_ASSERT      - static_assert()
-#  HAS_CXX11_VARIADIC_TEMPLATES - variadic templates
+#  HAS_CXX_AUTO                 - auto keyword
+#  HAS_CXX_CLASS_OVERRIDE       - override and final keywords for classes and methods
+#  HAS_CXX_CONSTEXPR            - constexpr keyword
+#  HAS_CXX_CSTDINT_H            - cstdint header
+#  HAS_CXX_DECLTYPE             - decltype keyword
+#  HAS_CXX_FUNC                 - __func__ preprocessor constant
+#  HAS_CXX_INITIALIZER_LIST     - initializer list
+#  HAS_CXX_LAMBDA               - lambdas
+#  HAS_CXX_LONG_LONG            - long long signed & unsigned types
+#  HAS_CXX_NULLPTR              - nullptr
+#  HAS_CXX_RVALUE_REFERENCES    - rvalue references
+#  HAS_CXX_SIZEOF_MEMBER        - sizeof() non-static members
+#  HAS_CXX_STATIC_ASSERT        - static_assert()
+#  HAS_CXX_VARIADIC_TEMPLATES   - variadic templates
 
 #=============================================================================
-# Copyright 2011,2012 Rolf Eike Beer <eike@sf-mail.de>
+# Copyright 2011,2012,2013 Rolf Eike Beer <eike@sf-mail.de>
 # Copyright 2012 Andreas Weis
 #
 # Distributed under the OSI-approved BSD License (the "License");
@@ -34,7 +34,7 @@
 #  License text for the above reference.)
 
 if (NOT CMAKE_CXX_COMPILER_LOADED)
-    message(FATAL_ERROR "CheckCXX11Features modules only works if language CXX is enabled")
+    message(FATAL_ERROR "CheckCXXFeatures modules only works if language CXX is enabled")
 endif ()
 
 cmake_minimum_required(VERSION 2.8.3)
@@ -44,23 +44,22 @@ cmake_minimum_required(VERSION 2.8.3)
 #
 include(CheckCXXCompilerFlag)
 check_cxx_compiler_flag("-std=c++11" _HAS_CXX11_FLAG)
-if (NOT _HAS_CXX11_FLAG)
-    check_cxx_compiler_flag("-std=c++0x" _HAS_CXX0X_FLAG)
-endif ()
-
 if (_HAS_CXX11_FLAG)
     set(CXX11_COMPILER_FLAGS "-std=c++11")
-elseif (_HAS_CXX0X_FLAG)
-    set(CXX11_COMPILER_FLAGS "-std=c++0x")
+else ()
+    check_cxx_compiler_flag("-std=c++0x" _HAS_CXX0X_FLAG)
+    if (_HAS_CXX0X_FLAG)
+        set(CXX11_COMPILER_FLAGS "-std=c++0x")
+    endif ()
 endif ()
 
-function(cxx11_check_feature FEATURE_NAME RESULT_VAR)
+function(cxx_check_feature FEATURE_NAME RESULT_VAR)
     if (NOT DEFINED ${RESULT_VAR})
-        set(_bindir "${CMAKE_CURRENT_BINARY_DIR}/cxx11_${FEATURE_NAME}")
+        set(_bindir "${CMAKE_CURRENT_BINARY_DIR}/cxx_${FEATURE_NAME}")
 
-        set(_SRCFILE_BASE ${CMAKE_CURRENT_LIST_DIR}/CheckCXX11Features/cxx11-test-${FEATURE_NAME})
+        set(_SRCFILE_BASE ${CMAKE_CURRENT_LIST_DIR}/CheckCXXFeatures/cxx11-test-${FEATURE_NAME})
         set(_LOG_NAME "\"${FEATURE_NAME}\"")
-        message(STATUS "Checking C++11 support for ${_LOG_NAME}")
+        message(STATUS "Checking C++ support for ${_LOG_NAME}")
 
         set(_SRCFILE "${_SRCFILE_BASE}.cpp")
         set(_SRCFILE_FAIL "${_SRCFILE_BASE}_fail.cpp")
@@ -104,25 +103,25 @@ function(cxx11_check_feature FEATURE_NAME RESULT_VAR)
         endif (${RESULT_VAR} AND EXISTS ${_SRCFILE_FAIL_COMPILE})
 
         if (${RESULT_VAR})
-            message(STATUS "Checking C++11 support for ${_LOG_NAME}: works")
+            message(STATUS "Checking C++ support for ${_LOG_NAME}: works")
         else (${RESULT_VAR})
-            message(STATUS "Checking C++11 support for ${_LOG_NAME}: not supported")
+            message(STATUS "Checking C++ support for ${_LOG_NAME}: not supported")
         endif (${RESULT_VAR})
-        set(${RESULT_VAR} ${${RESULT_VAR}} CACHE INTERNAL "C++11 support for ${_LOG_NAME}")
+        set(${RESULT_VAR} ${${RESULT_VAR}} CACHE INTERNAL "C++ support for ${_LOG_NAME}")
     endif (NOT DEFINED ${RESULT_VAR})
-endfunction(cxx11_check_feature)
+endfunction(cxx_check_feature)
 
-cxx11_check_feature("static_assert" HAS_CXX11_STATIC_ASSERT)
-cxx11_check_feature("long_long" HAS_CXX11_LONG_LONG)
-cxx11_check_feature("auto" HAS_CXX11_AUTO)
-cxx11_check_feature("rvalue-references" HAS_CXX11_RVALUE_REFERENCES)
-cxx11_check_feature("constexpr" HAS_CXX11_CONSTEXPR)
-cxx11_check_feature("sizeof_member" HAS_CXX11_SIZEOF_MEMBER)
-cxx11_check_feature("__func__" HAS_CXX11_FUNC)
-cxx11_check_feature("nullptr" HAS_CXX11_NULLPTR)
-cxx11_check_feature("cstdint" HAS_CXX11_CSTDINT_H)
-cxx11_check_feature("initializer_list" HAS_CXX11_INITIALIZER_LIST)
-cxx11_check_feature("class_override_final" HAS_CXX11_CLASS_OVERRIDE)
-cxx11_check_feature("decltype" HAS_CXX11_DECLTYPE)
-cxx11_check_feature("lambda" HAS_CXX11_LAMBDA)
-cxx11_check_feature("variadic_templates" HAS_CXX11_VARIADIC_TEMPLATES)
+cxx_check_feature("static_assert" HAS_CXX_STATIC_ASSERT)
+cxx_check_feature("long_long" HAS_CXX_LONG_LONG)
+cxx_check_feature("auto" HAS_CXX_AUTO)
+cxx_check_feature("rvalue-references" HAS_CXX_RVALUE_REFERENCES)
+cxx_check_feature("constexpr" HAS_CXX_CONSTEXPR)
+cxx_check_feature("sizeof_member" HAS_CXX_SIZEOF_MEMBER)
+cxx_check_feature("__func__" HAS_CXX_FUNC)
+cxx_check_feature("nullptr" HAS_CXX_NULLPTR)
+cxx_check_feature("cstdint" HAS_CXX_CSTDINT_H)
+cxx_check_feature("initializer_list" HAS_CXX_INITIALIZER_LIST)
+cxx_check_feature("class_override_final" HAS_CXX_CLASS_OVERRIDE)
+cxx_check_feature("decltype" HAS_CXX_DECLTYPE)
+cxx_check_feature("lambda" HAS_CXX_LAMBDA)
+cxx_check_feature("variadic_templates" HAS_CXX_VARIADIC_TEMPLATES)
--- Tests/Module/CXXFeatures/cxxfeatures.cxx
+++ Tests/Module/CXXFeatures/cxxfeatures.cxx
@@ -1,4 +1,4 @@
-#if defined(HAS_CXX0X_CSTDINT_H)
+#if defined(HAS_CXX_CSTDINT_H)
 #include <cstdint>
 #endif
 
@@ -6,10 +6,10 @@
 
 struct thing {
     unsigned char one;
-#if defined(HAS_CXX0X_CSTDINT_H)
+#if defined(HAS_CXX_CSTDINT_H)
     uint32_t four;
 #endif
-#if defined(HAS_CXX0X_LONG_LONG)
+#if defined(HAS_CXX_LONG_LONG)
     long long eight;
 #endif
 };
@@ -18,40 +18,40 @@ struct thing {
 
 int main()
 {
-#if defined (HAS_CXX0X_NULLPTR)
+#if defined (HAS_CXX_NULLPTR)
     void *nix = nullptr;
-#else /* HAS_CXX0X_NULLPTR */
+#else /* HAS_CXX_NULLPTR */
     void *nix = 0;
-#endif /* HAS_CXX0X_NULLPTR */
+#endif /* HAS_CXX_NULLPTR */
 
-#if defined(HAS_CXX0X_STATIC_ASSERT)
+#if defined(HAS_CXX_STATIC_ASSERT)
     static_assert(1 < 42, "Your C++ compiler is b0rked");
-#endif /* HAS_CXX0X_STATIC_ASSERT */
+#endif /* HAS_CXX_STATIC_ASSERT */
 
-#if defined(HAS_CXX0X_FUNC)
+#if defined(HAS_CXX_FUNC)
     const char *funcname = __func__;
     printf("the name of main() function is: %s\n", funcname);
-#endif /* HAS_CXX0X_FUNC */
+#endif /* HAS_CXX_FUNC */
 
-#if defined(HAS_CXX0X_SIZEOF_MEMBER)
+#if defined(HAS_CXX_SIZEOF_MEMBER)
     size_t onesize = sizeof(thing::one);
-#if defined(HAS_CXX0X_STATIC_ASSERT)
+#if defined(HAS_CXX_STATIC_ASSERT)
     static_assert(sizeof(thing::one) == 1, "Your char is not one byte long");
-#endif /* HAS_CXX0X_STATIC_ASSERT */
+#endif /* HAS_CXX_STATIC_ASSERT */
 
-#if defined(HAS_CXX0X_CSTDINT_H)
+#if defined(HAS_CXX_CSTDINT_H)
     size_t foursize = sizeof(thing::four);
-#if defined(HAS_CXX0X_STATIC_ASSERT)
+#if defined(HAS_CXX_STATIC_ASSERT)
     static_assert(sizeof(thing::four) == 4, "Your uint32_t is not 32 bit long");
-#endif /* HAS_CXX0X_STATIC_ASSERT */
-#endif /* HAS_CXX0X_CSTDINT_H */
-#if defined(HAS_CXX0X_LONG_LONG)
+#endif /* HAS_CXX_STATIC_ASSERT */
+#endif /* HAS_CXX_CSTDINT_H */
+#if defined(HAS_CXX_LONG_LONG)
     size_t eightsize = sizeof(thing::eight);
-#if defined(HAS_CXX0X_STATIC_ASSERT)
+#if defined(HAS_CXX_STATIC_ASSERT)
     static_assert(sizeof(thing::eight) == 8, "Your long long is not 64 bit long");
-#endif /* HAS_CXX0X_STATIC_ASSERT */
-#endif /* HAS_CXX0X_LONG_LONG */
-#endif /* HAS_CXX0X_SIZEOF_MEMBER */
+#endif /* HAS_CXX_STATIC_ASSERT */
+#endif /* HAS_CXX_LONG_LONG */
+#endif /* HAS_CXX_SIZEOF_MEMBER */
 
     return 0;
 }
